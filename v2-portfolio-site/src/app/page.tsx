fc76f47d54f0dda54cc71dfa4ebd15dea979dff9
Richard <richardma1999@gmail.com>
2024-02-05 15:47:59 -0500

    worked on main layout of site

--- v2-portfolio-site/src/app/page.tsx
+++ v2-portfolio-site/src/app/page.tsx
@@ -2,31 +2,62 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="font-mono">
-      <div className="flex">
-        <div className="w-1/2">
-          <h1>Richard Ma</h1>
-          <h2>Web Developer</h2>
-          <h3>HBSC Computer Science</h3>
-        </div>
-        <div className="w-1/2">
-          <div>
-            Hello, my name is Richard! <br/>
-            As a software engineer, I am fueled by my passion for technology and my love 
-            for learning and solving new challenges. I have professional experience 
-            building responsive React webpages, and in depth back-end knowledge on data 
-            structures, algorithms, and databases gained during my undergraduate studies. 
-            I am constantly seeking out new opportunities to grow my skills and knowledge, 
-            and I approach each challenge with enthusiasm and curiosity.
+    <main className="font-mono h-full bg-slate-900 text-slate-200">
+      <div className="px-20 h-full">
+        <div className="flex h-full">
+          <div className="w-1/2 flex flex-col justify-between py-10 sticky top-0 h-screen">
+            <div>
+              <h1 className="text-6xl">Richard Ma</h1>
+              <h2 className="text-2xl">Web Developer</h2>
+              <h3>HBSC Computer Science</h3>
+            </div>
           </div>
-          <div>
-            Experience
-          </div>
-          <div>
-            Skills
-          </div>
-          <div>
-            Projects
+          <div className="w-1/2 text-slate-400 py-10">
+            <div>
+              Hello, my name is Richard! <br/>
+              As a software engineer, I am fueled by my passion for technology and my love 
+              for learning and solving new challenges. I have professional experience 
+              building responsive React webpages, and in depth back-end knowledge on data 
+              structures, algorithms, and databases gained during my undergraduate studies. 
+              I am constantly seeking out new opportunities to grow my skills and knowledge, 
+              and I approach each challenge with enthusiasm and curiosity.
+            </div>
+            <div>
+              Hello, my name is Richard! <br/>
+              As a software engineer, I am fueled by my passion for technology and my love 
+              for learning and solving new challenges. I have professional experience 
+              building responsive React webpages, and in depth back-end knowledge on data 
+              structures, algorithms, and databases gained during my undergraduate studies. 
+              I am constantly seeking out new opportunities to grow my skills and knowledge, 
+              and I approach each challenge with enthusiasm and curiosity.
+            </div>
+            <div>
+              Hello, my name is Richard! <br/>
+              As a software engineer, I am fueled by my passion for technology and my love 
+              for learning and solving new challenges. I have professional experience 
+              building responsive React webpages, and in depth back-end knowledge on data 
+              structures, algorithms, and databases gained during my undergraduate studies. 
+              I am constantly seeking out new opportunities to grow my skills and knowledge, 
+              and I approach each challenge with enthusiasm and curiosity.
+            </div>
+            <div>
+              Hello, my name is Richard! <br/>
+              As a software engineer, I am fueled by my passion for technology and my love 
+              for learning and solving new challenges. I have professional experience 
+              building responsive React webpages, and in depth back-end knowledge on data 
+              structures, algorithms, and databases gained during my undergraduate studies. 
+              I am constantly seeking out new opportunities to grow my skills and knowledge, 
+              and I approach each challenge with enthusiasm and curiosity.
+            </div>
+            <div>
+              Experience
+            </div>
+            <div>
+              Skills
+            </div>
+            <div>
+              Projects
+            </div>
           </div>
         </div>
       </div>
