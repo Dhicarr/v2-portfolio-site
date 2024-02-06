41286c491bd62af9bad7067af7b793521e11957e
Richard <richardma1999@gmail.com>
2024-02-06 14:03:15 -0500

    Update page.tsx

--- v2-portfolio-site/src/app/page.tsx
+++ v2-portfolio-site/src/app/page.tsx
@@ -8,35 +8,44 @@ export default function Home() {
         <div className="flex h-full">
           <div className="w-1/2 flex flex-col justify-around py-10 sticky top-0 h-screen">
             <div>
-              <h1 className="text-6xl">Richard Ma</h1>
+              <h1 className="text-6xl font-semibold">Richard Ma</h1>
               <h2 className="text-2xl">Web Developer</h2>
-              <h3>HBSC Computer Science</h3>
+              <h3>UofT - HBSc Computer Science</h3>
+              <div>resume</div>
             </div>
             <div>
-              List
+              <ul>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Skills</li>
+                <li>Projects</li>
+              </ul>
             </div>
             <div>
               Icons Icons Icons
             </div>
           </div>
-          <div className="w-1/2 text-slate-400 py-24 flex flex-col space-y-4">
-            <div>
+          <div className="w-1/2 text-slate-400 py-24 flex flex-col space-y-20">
+            <div className="leading-relaxed">
               <h2 className="text-slate-200 text-2xl">About Me</h2>
-              Hello, my name is Richard! <br/>
-              As a software engineer, I am fueled by my passion for technology and my love 
-              for learning and solving new challenges. I have professional experience 
-              building responsive React webpages, and in depth back-end knowledge on data 
-              structures, algorithms, and databases gained during my undergraduate studies. 
-              I am constantly seeking out new opportunities to grow my skills and knowledge, 
-              and I approach each challenge with enthusiasm and curiosity.
+              <div>
+                Hello, my name is Richard! <br/>
+                As a software engineer, I am fueled by my passion for technology and my love 
+                for learning and solving new challenges. I have professional experience 
+                building responsive React webpages, and in depth back-end knowledge on data 
+                structures, algorithms, and databases gained during my undergraduate studies. 
+                I am constantly seeking out new opportunities to grow my skills and knowledge, 
+                and I approach each challenge with enthusiasm and curiosity.
+              </div>
             </div>
             <div className="flex flex-col space-y-4">
               <h2 className="text-slate-200 text-2xl">Experience</h2>
+              <h3 className="text-slate-300 text-xl mt-6">DeckerMed</h3>
               <div className="flex flex-col">
                 <div className="flex">
-                  <div className="basis-1/4 font-mono text-sm">Aug 2022-May 2023</div>
-                  <div className="basis-3/4">
-                    Intermediate Front End Developer - DeckerMed
+                  <div className="basis-1/4 font-mono text-sm">Aug 2022 — May 2023</div>
+                  <div className="basis-3/4 pl-4">
+                    <div className="text-slate-300">Intermediate Front End Developer</div>
                     <div>
                       • Responsible for rebuilding 35% of total company web pages and applications using React, utilized by over 25k
                       unique monthly users from medical programs at top universities and institutions.
@@ -53,11 +62,11 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex">
-                <div className="basis-1/4 font-mono text-sm">Jun 2022-Aug 2022</div>
-                <div className="basis-3/4">
-                    Junior Full Stack Developer - DeckerMed
+                </div>
+                <div className="flex">
+                <div className="basis-1/4 font-mono text-sm">Jun 2022 — Aug 2022</div>
+                <div className="basis-3/4 pl-4">
+                    <div className="text-slate-300">Junior Full Stack Developer</div>
                     <div>
                       • Contributed to large-scale critical email template sent out to users weekly using CSS, HTML, and Django.
                       <br/>
@@ -66,8 +75,8 @@ export default function Home() {
                       • Created a new React page to replace outdated vanilla JavaScript page in production.
                       <br/>
                       • Implemented API routes with Django.
-                    </div>
-                  </div>
+                </div>
+              </div>
               </div>
             </div>
             <div>
